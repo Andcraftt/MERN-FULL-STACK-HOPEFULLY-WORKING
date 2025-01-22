12e59975e83dfe3b53bc7545b7484f789c2ded60
Andcraftt <andcraftt@gmail.com>
2025-01-22 17:10:54 +0100

    Update authSlice.js

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
-import authService from './authService'
+import authService from 'https://mern-full-stack-1-0.onrender.com/authService'
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem('user'))
